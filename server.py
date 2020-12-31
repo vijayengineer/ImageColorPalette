ef6ead9624d68dc62b1e41adcb7abbe7634ce02e
Vijay <37715711+vijayengineer@users.noreply.github.com>
2020-12-31 12:52:36 +0000

    Tested files

---
+++ server.py
@@ -0,0 +1,52 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort, \
+    send_from_directory
+from PIL import Image
+import numpy as np
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 2*1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg','.jpeg','.png','.gif','.bmp']
+app.config['UPLOAD_PATH'] = 'uploads'
+
+
+@app.route('/')
+def index():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('index.html', files=files)
+
+@app.route('/', methods=['POST','GET'])
+def upload_convert():
+    filename = ''
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            return "Invalid file", 400
+        else:
+            uploaded_file.save('uploads/'+filename)
+            img = Image.open('uploads/'+filename)
+            img_array = np.array(img)
+            colors, count = np.unique(img_array.reshape(-1, img_array.shape[-1]), axis=0, return_counts=True)
+            top10 = colors[count.argsort()[-10:]].tolist()
+            top10_hex = ['#%02x%02x%02x' % (c[0], c[1], c[2]) for c in top10]
+            print(top10_hex)
+            return render_template('upload_convert.html', top10=top10_hex)
+    return redirect(url_for('index'))
+   
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
